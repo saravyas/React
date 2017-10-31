736e95e748373aea5b573121181afbd996b75450
saravyas <saravyas007@gmail.com>
2017-10-31 10:39:38 +0530

    Header Footer Title component added

--- app/js/components/Header.js
+++ app/js/components/Header.js
@@ -1,9 +1,12 @@
 import React  from 'react';
+
+import Title from './Header/Title'
+
 export default class Header extends React.Component {
 	render(){
 		return(
 			<div>
-				<header>Header</header>	
+				<Title/>
  			</div>
 		)
 	}
