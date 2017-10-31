61c8f6516760ba94eb615ae371d933fcfa440a95
saravyas <saravyas007@gmail.com>
2017-10-31 07:49:24 +0530

    Component with constructor

--- app/js/Example.js
+++ app/js/Example.js
@@ -1,23 +1,13 @@
 import React , {Component} from 'react';
 export default class Example extends Component {
-	value(){
-		return 'saravyas';
-	}
-	temp(name){
-		return 'gomal' +name;
+	constructor(){
+		super();
+		this.name = 'sara';
 	}
 	render(){
-		const i=1;
-		const name = "sara";
 		return(
 			<div>
-				<p>{this.value()}</p>
-				<p>{this.temp('vyas')}</p>
-				<p>my name is {name} </p>
-				<h1>sara</h1>
-				<p>{2+3}</p>
-				<p>{i===1?'true':'false'}</p>
-				<p>{(function() {return 3;})()}</p>
+				<p>{this.name}</p>
 			</div>
 		)
 	}
