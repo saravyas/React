71411aa38cf88bbe5f8000c67392d0c27f8ea0ac
saravyas <saravyas007@gmail.com>
2017-10-31 10:21:38 +0530

    Component base app

---
+++ app/client.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ 
+import Layout from './components/Layout';
+// import Example from "./Example"
+
+ 
+ 
+
+const app =document.getElementById('app');
+
+// ReactDOM.render(<Layout/>,app);
+ReactDOM.render(<Layout/>,app);
