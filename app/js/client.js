1db40ad1fac435fc46f6c6ac2b30871df868fb62
saravyas <saravyas007@gmail.com>
2017-10-31 10:25:56 +0530

    client file for component

--- app/js/client.js
+++ app/js/client.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
  
-import Example from './Example';
+import Layout from './components/Layout';
+// import Example from "./Example"
 
  
  
 
 const app =document.getElementById('app');
 
-ReactDOM.render(<Example/>,app);
+// ReactDOM.render(<Layout/>,app);
+ReactDOM.render(<Layout/>,app);
