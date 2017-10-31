fccc09985fc9caf9462615cc673d03180281aeba
saravyas <saravyas007@gmail.com>
2017-10-31 10:44:35 +0530

    simple state example with static data

--- app/js/components/Layout.js
+++ app/js/components/Layout.js
@@ -4,9 +4,14 @@ import Header from './Header';
 import Footer from './Footer';
 
 export default class Layout extends React.Component {
+	constructor(){
+		super();
+		this.state = {name:"sara"}
+	}
 	render(){
 		return(
 			<div>
+				{this.state.name}
 				<Header/>
 			 	<Footer/>
 			</div>
